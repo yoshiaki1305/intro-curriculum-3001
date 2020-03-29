35f5d37f79632aa730e2e235741342bbc8bb6df9
yoshiaki1305 <yoshiakikawamoto823@gmail.com>
2020-03-29 23:46:04 +0900

    first

--- app.js
+++ app.js
@@ -7,12 +7,18 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for (let x = n;x>1;x--) {
+        console.log(x);
+        result = result * x;
+        console.log(result)
+    }
+
     return result;
 }
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+console.log(factorial(3))
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
